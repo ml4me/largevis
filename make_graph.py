243328f51705b10960375e87beabae20824f5e39
Tim Emerick <temerick@gmail.com>
2016-08-06 15:53:36 -0400

    fixed neighbor of neighbor bug

--- make_graph.py
+++ make_graph.py
@@ -5,32 +5,34 @@ from heapq import heappush, heappop, heappushpop, nsmallest
 
 np.random.seed(42)
 def lvnn(fp, nt=3, k=5, iter= 2, leaves=50):
-    
+
     nn=np.zeros((fp.shape[0],k), dtype='int')-1
     nn_d=np.zeros((fp.shape[0],k))-1
-    
+
     print ' start Tree build'
     model = RPForest(leaf_size=leaves, no_trees=nt)
     model.fit(fp)
     for i in range(0,fp.shape[0]):
         nn[i,:] =model.query(fp [i,],k)
-    
+
     t=0
     print 'startnn'
-    ii=[i]*(k*k)
     while t<iter:
         t +=1
+        print 'current iter:', t
         old_nn=nn
         for i in range(0,fp.shape[0]):
             ji = old_nn[i,:]
             li = old_nn[ji,:]
-            jli = li.flatten()
-            d=np.linalg.norm(fp [ii,:]-fp [jli,:], axis=1)
-            mind=np.argpartition(d,k)
-            nn [i,:]=jli[mind[:k]]
-            nn_d [i,:]=d[mind[:k]]
-            
-    
+            jli = np.unique(li.flatten())
+            jli = jli[(jli != i)]
+            d=np.linalg.norm(fp [i,:]-fp [jli,:], axis=1)
+            d_idxs = np.argsort(d)[:k]
+            # try:
+            nn [i,:]=jli[d_idxs]
+            nn_d [i,:]=d[d_idxs]
+
+
     csr=np.zeros((fp.shape[0]*k, 3))
     print 'start split'
     l=0
@@ -57,6 +59,7 @@ def testit():
     
     lvnn(fp, k=k)
     print ' time',(time.time()*1000.0)-ms
-    
-import cProfile
-st=cProfile.run('testit()', filename='stat.txt', sort='cumtime')
+
+if __name__ == '__main__':
+    import cProfile
+    st=cProfile.run('testit()', filename='stat.txt', sort='cumtime')
